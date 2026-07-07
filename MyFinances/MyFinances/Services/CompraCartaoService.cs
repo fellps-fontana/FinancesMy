c43ec385b81e283630c3c7835f2de458038cc0c0
Felipe Locatelli Fontana <fefe.giva13@gmail.com>
2026-07-06 22:50:21 -0300

    Reapply "Padroniza pasta e namespace de DTOs para DTOs (era Dtos)"
    This reverts commit ad50a49278947d596cc76c0a3cf0330ffde0061e.

--- MyFinances/MyFinances/Services/CompraCartaoService.cs
+++ MyFinances/MyFinances/Services/CompraCartaoService.cs
@@ -1,7 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using MyFinances.Data;
 using MyFinances.Domain;
-using MyFinances.Dtos;
+using MyFinances.DTOs;
 using MyFinances.Models;
 
 namespace MyFinances.Services;
